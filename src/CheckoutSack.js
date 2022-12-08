2ec95f9a933476fdfb2bd95f8f14c3ccb4cfb98e
Angelo Perpetua <jose.perpetua@usjr.edu.ph>
2022-12-08 15:45:36 +0800

    Checkout
    Hinay hinayan lang sa nako continue ko later

--- src/CheckoutSack.js
+++ src/CheckoutSack.js
@@ -1,26 +1,34 @@
-import React from "react";
-import { useState } from "react";
-import "./css/shoppingSack.css";
-import items from "./ItemsData";
-import { AiFillCloseCircle } from "react-icons/ai";
-import { RiDeleteBin6Line } from "react-icons/ri";
 
-const CheckoutSack = ({ setShowCheckout }) => {
-  return (
-    <div className="modal">
-      <div className="shoppingCart">
-        <div className="header">
-          <h2>Shopping Sack</h2>
-          <button className="btn close-btn">
-            <AiFillCloseCircle size={30} />
-          </button>
-        </div>
-        <div className="cart-products">
-          <button className="btn checkout-btn">Proceed to checkout</button>
-        </div>
-      </div>
-    </div>
-  );
+import React from 'react'; 
+
+const CheckOut = () => {
+	
+	return(
+		<>
+			<h1>Shipping Address</h1>
+			<div>
+			Name Name 09330239495 <br />
+			Magallanes Street, 6000 Cebu City, Philippines
+			</div>
+			<h2>SackO</h2>
+			<h3> Shipping Method</h3>
+			<h4> STANDARD SHIPPING</h4>
+			<div>
+			₱0(Orders placed now are expected to arrive before Wednesday,<br/>
+			Dec 14 - Saturday, Dec 17. The actual delivery time is subject to the official website/ notice of the logistics company.)
+			</div>
+			<h4> Shipping Guarantee: ₱0  </h4>
+			<h4> Total: ₱  </h4>
+			
+			<h2>Payment Method</h2>
+
+      	
+		
+			
+
+
+		</>
+	);
 };
 
-export default CheckoutSack;
+export default CheckOut;
